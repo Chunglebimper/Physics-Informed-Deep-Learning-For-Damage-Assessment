b84437bc485c84d7a5a3d13ea7e734e29b880196
crota-prod <caidenfp@gmail.com>
2025-06-23 11:52:56 -0400

    Changed log format, added new parameters (levels and sample_size)

--- src/main.py
+++ src/main.py
@@ -14,6 +14,9 @@ if __name__ == "__main__":
     parser.add_argument('--epochs', type=int, default=20, help='Number of training epochs')
     parser.add_argument('--lr', type=float, default=1e-6, help='Learning rate')
     parser.add_argument('--data_root', type=str, required=True, help='Path to dataset root directory')
+    parser.add_argument('--verbose', action='store_true', help='Includes Epoch data in log')
+    parser.add_argument('--sample_size', type=int,  default=128, help='Amount of images sampled')
+    parser.add_argument('--levels', type=int, default=32, help='Levels in powers of two')
     args = parser.parse_args()
 
     start_time = time.perf_counter()  # Record the start time           PART OF TIME FUNCTION
@@ -26,7 +29,10 @@ if __name__ == "__main__":
         batch_size=args.batch_size,
         epochs=args.epochs,
         lr=args.lr,
-        root=args.data_root
+        root=args.data_root,
+        verbose=args.verbose,
+        sample_size=args.sample_size,
+        levels=args.levels
     )
     # ---------------------------------
 
@@ -35,4 +41,8 @@ if __name__ == "__main__":
     hours = int(elapsed_time // 3600)  # PART OF TIME FUNCTION
     minutes = int((elapsed_time % 3600) // 60)  # PART OF TIME FUNCTION
     seconds = int(elapsed_time % 60)  # PART OF TIME FUNCTION
-    print(f"Total elapsed time: {hours: >6} hours, {minutes: >6} minutes, {seconds: >6} seconds")  # PART OF TIME FUNCTION
+
+    log_instance=Log()
+    log_instance.open(override=True)
+    log_instance.append(f"Total elapsed time: {hours: >6} hours, {minutes: >6} minutes, {seconds: >6} seconds")
+    log_instance.close()
