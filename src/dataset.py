6e016d44fe3ab86c10b545184346b81b09389836
Henry Stern <26hstern@hra.org>
2025-07-23 14:00:13 -0400

    fixed a percentage error

--- src/dataset.py
+++ src/dataset.py
@@ -40,7 +40,7 @@ class DamageDataset(Dataset):
             for y in range(0, h - patch_size + 1, stride):
                 for x in range(0, w - patch_size + 1, stride):
                     patch = mask[y:y + patch_size, x:x + patch_size]
-                    include = (4 in patch or 3 in patch or 2 in patch or np.random.rand() < float(class0and1percent*0.1))
+                    include = (4 in patch or 3 in patch or 2 in patch or np.random.rand() < float(class0and1percent*0.01))
                     if include:
                         path = os.path.join(post_dir, f'{basename}_post_disaster.png')
                         img = Image.open(path)
